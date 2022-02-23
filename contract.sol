9bd256efaebd8f427251ff2bd83a0d55bf4938fd
pe1l1nl1 <19546253+pe1l1nl1@users.noreply.github.com>
2022-02-24 00:06:00 +0200

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,24 +1,35 @@
-//cryptolychees
 
+//CRYPTOZOMBIES
 
 pragma solidity >=0.5.0 <0.6.0;
 
-contract LycheeGarden {
+contract ZombieFactory {
+
+    // declare our event here
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
-    struct Lychee {
+    struct Zombie {
         string name;
         uint dna;
     }
 
-    Zombie[] public Lychees;
+    Zombie[] public zombies;
 
-    function _createLychee(string memory _name, uint _dna) private {
-        Lychees.push(Lychee(_name, _dna));
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+        // and fire it here
     }
 
-    // start here
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
 
 }
